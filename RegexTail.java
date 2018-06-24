0bc33fd456d870d93b34c50eaf01d47db419e61a
kbmaster <kbmaster231281@hotmail.com>
2018-06-24 19:43:13 -0300

    add comments

--- RegexTail.java
+++ RegexTail.java
@@ -17,6 +17,9 @@ class RegexTail implements  Tail
                 this.probability=p;
         }
 
+	//esto retorna una lista de simbolos a partir de una expresion regular
+	//la lista de simbolos deben ser todos terminales en orden tal que garantice 
+	//que la cadena pertenece al lenguaje que describe la regex	
 	public List <BabbleSymbol> produce()
 	{
 		return null;
